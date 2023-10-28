b99e7cb4c42991a83de5c9409cdeac2352ae370c
damann8479 <info@mpknails.de>
2023-10-28 10:34:28 +0200

    Vorlage done

---
+++ resources/views/components/myComponents/table/heading.blade.php
@@ -0,0 +1,3 @@
+<th {{ $attributes->merge(['class' => 'px-5 py-3 border-b-2 border-gray-200 text-left bg-gray-800']) }}>
+  <span class="text-left text-xs leading-4  font-medium text-white uppercase tracking-wider"> {{ $slot }} </span>
+</th>
